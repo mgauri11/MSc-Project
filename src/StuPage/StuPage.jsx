110613903200084a355230e5bc8048fd7f4077f1
mgauri2109 <mudgauri11@gmail.com>
2020-10-12 18:07:52 +0100

    tweaked logic for module data

--- src/StuPage/StuPage.jsx
+++ src/StuPage/StuPage.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios"
-import Header from './Header/Header'
+//import Header from './Header/Header'
 import './StuPage.css'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 const StuPage = () => {
 
@@ -9,24 +10,36 @@ const StuPage = () => {
 
     useEffect(() => {
         const fecthData = async () => {
-            await axios.get("http://localhost:5000/api/auth/info")
+            await axios.get("/api/info")
                 .then(res => {
                     setCountries(res.data);
-                    console.log(countries);
+                    console.log(res.data);
                 });
         };
         fecthData();
+        
     }, []);
+    //console.log(countries)
     
 
     return (
         <div  className='title'>
-            <Header />
+            <h2>Student Page</h2>
             <div className='login-root'>
                 <img src={require('./calender.jpg')} alt='cool image coming soon!!!! ;)' width="300" height="250" ></img>
                 
             </div>
+            {countries?.map((number) =>
+            <div>
+
+                <h6>{number.Degree}</h6>
+                <h6>{number.Degree_Program}</h6>
+                <h6>{number.Semester}</h6>
+                <h6>{number.Module_name}</h6>
+    
+            </div>)} 
             <p className='p1'>Slots available</p>
+            <button>Continue</button>
             
         </div>
     ) 
